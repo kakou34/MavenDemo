d2fa4c1c7c0408b403677f16e30db844e8b3c2c4
kakou34 <kaouther.mouheb@gmail.com>
2018-10-16 17:20:48 +0300

    This is a new commit for what I originally planned to be amended

---
+++ src/test/java/MyPackage/AppTest.java
@@ -0,0 +1,20 @@
+package MyPackage;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
